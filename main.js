df39f974281798a48b98cdb23a3d74ba5bc2e521
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-04-08 11:42:01 +0300

    fetch advice api

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 const adviceNumberEl = document.querySelector('#advice-number');
 const adviceTextEl = document.querySelector('#advice-text');
+
+fetch('https://api.adviceslip.com/advice')
+  .then((res) => res.json())
+  .then((data) => console.log(data));
