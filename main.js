6ceb6be0b482b544e47e4ed9ac3756e39711202e
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-04-08 12:00:58 +0300

    create function to update text content of advice text and id number

--- main.js
+++ main.js
@@ -2,12 +2,16 @@ const adviceNumberEl = document.querySelector('#advice-number');
 const adviceTextEl = document.querySelector('#advice-text');
 const generateAdviceBtn = document.querySelector('.generator-btn');
 
+const updateTextContent = (id, text) => {
+  adviceNumberEl.textContent = id;
+  adviceTextEl.textContent = text;
+};
+
 const getRandomAdvice = () => {
   fetch('https://api.adviceslip.com/advice')
     .then((res) => res.json())
     .then((data) => {
-      adviceNumberEl.textContent = data.slip.id;
-      adviceTextEl.textContent = data.slip.advice;
+      updateTextContent(data.slip.id, data.slip.advice);
     });
 };
 
