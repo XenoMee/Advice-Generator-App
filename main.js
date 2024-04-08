5a07f314ddd7aae02d39e1b6e7a53ce90ff323cd
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-04-08 11:46:48 +0300

    place the fetch API inside a function

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const adviceNumberEl = document.querySelector('#advice-number');
 const adviceTextEl = document.querySelector('#advice-text');
 
-fetch('https://api.adviceslip.com/advice')
-  .then((res) => res.json())
-  .then((data) => {
-    adviceNumberEl.textContent = data.slip.id;
-    adviceTextEl.textContent = data.slip.advice;
-  });
+const getRandomAdvice = () => {
+  fetch('https://api.adviceslip.com/advice')
+    .then((res) => res.json())
+    .then((data) => {
+      adviceNumberEl.textContent = data.slip.id;
+      adviceTextEl.textContent = data.slip.advice;
+    });
+};
